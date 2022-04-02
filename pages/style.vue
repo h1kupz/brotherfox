55ea99acb683313627ed4b6f2639d6065bf3bb53
h1kupz <h1kupz3z@gmail.com>
2022-04-02 12:38:08 +0700

    Attempt Fix CMS Enter Anim2

--- pages/style.vue
+++ pages/style.vue
@@ -41,7 +41,7 @@
       <content>
         <div
           class="fox-grid opacity-0 translate-y-1/4 duration-1000"
-          x-intersect.threshold.1="$el.classList.add('opacity-100', 'translate-y-0')"
+          x-intersect.margin.200px="$el.classList.add('opacity-100', 'translate-y-0')"
           x-intersect:leave="$el.classList.remove('opacity-100', 'translate-y-0')"
         >
           <h1 class="col-span-full">CMS</h1>
