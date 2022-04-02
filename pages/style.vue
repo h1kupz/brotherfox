0b31969a2b53121d088f631113345effad603f51
h1kupz <h1kupz3z@gmail.com>
2022-04-02 12:06:31 +0700

    Attempt Fix CMS Enter Anim

--- pages/style.vue
+++ pages/style.vue
@@ -38,12 +38,12 @@
       </content>
     </section>
     <section id="cms" class="bg-bone">
-      <content
-        class="opacity-0 translate-y-1/4 duration-1000"
-        x-intersect.threshold.1="$el.classList.add('opacity-100', 'translate-y-0')"
-        x-intersect:leave="$el.classList.remove('opacity-100', 'translate-y-0')"
-      >
-        <div class="fox-grid">
+      <content>
+        <div
+          class="fox-grid opacity-0 translate-y-1/4 duration-1000"
+          x-intersect.threshold.1="$el.classList.add('opacity-100', 'translate-y-0')"
+          x-intersect:leave="$el.classList.remove('opacity-100', 'translate-y-0')"
+        >
           <h1 class="col-span-full">CMS</h1>
           <p class="col-span-full mb-4">
             Example of an editable section. Team members can be added, edited
