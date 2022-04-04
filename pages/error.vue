dbde6df3f01386ef63b02fcc205523540c14c115
h1kupz <h1kupz3z@gmail.com>
2022-04-04 11:08:05 +0700

    Error Take 2

--- pages/error.vue
+++ pages/error.vue
@@ -3,8 +3,8 @@
     <content>
       <div class="grid justify-center">
         <h1 v-if="error.statusCode === 404" class="text-9xl">F#!k, плут</h1>
-        <h1 v-else-if="error.statusCode === 500">F#!k, плут</h1>
-        <h1 v-else>F#!k, плут</h1>
+        <h1 v-else-if="error.statusCode === 500">Server error</h1>
+        <h1 v-else>Error Occurred</h1>
         <h4 class="justify-self-center hover:text-bone mt-6">
           <NuxtLink to="/style">Back to Bakunin</NuxtLink>
         </h4>
