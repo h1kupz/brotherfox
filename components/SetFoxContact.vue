8c3200b79d14c3a8a2abad179ecf1fa2d38b2bc4
h1kupz <h1kupz3z@gmail.com>
2022-04-05 11:10:27 +0700

    Form Config

--- components/SetFoxContact.vue
+++ components/SetFoxContact.vue
@@ -43,12 +43,7 @@
           class="fox-card bg-opacity-0 grid col-span-full lg:col-span-5 mb-2 sm:order-last lg:row-start-2"
         >
           <h4 class="col-span-full pb-2 dark:text-green">Message</h4>
-          <form
-            class="mb-0 w-full"
-            action="#"
-            method="POST"
-            data-netlify="true"
-          >
+          <form class="mb-0 w-full" action="#" method="POST" netlify>
             <div>
               <label for="email" class="block text-gold dark:text-green"
                 >Name</label
